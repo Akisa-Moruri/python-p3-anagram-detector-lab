ef94a2ce1070db0eb31b6942629dd9e37dcf9912
Akisa-Moruri <akisamoruri@gmail.com>
2024-05-27 10:24:07 +0300

    anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Convert the word to lowercase 
+
+    def match(self, words):
+        matches = []
+        for w in words:
+            # Check if the word is an anagram by comparing sorted letters
+            if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word:
+                matches.append(w)
+        return matches
